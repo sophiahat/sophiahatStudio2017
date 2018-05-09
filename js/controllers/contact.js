e8907586be2e263fa7ae603caf3386d0b6587651
Chris Spencer <chris@chrisspencercreative.com>
2018-05-08 23:09:29 -0700

    clean up clients and work on portfolio

--- js/controllers/contact.js
+++ js/controllers/contact.js
@@ -1,10 +1,10 @@
 myApp.controller('ContactController', ['$scope', function($scope) {
     $scope.message = 'Contact Controller';
     var url = window.location.href;
-    gtag('config', 'UA-20609405-2', {
-        'page_path' : '/#/contact',
-        'page_location' : url,
-        'page_title' : 'Contact'
-    
-    });
+//    gtag('config', 'UA-20609405-2', {
+//        'page_path' : '/#/contact',
+//        'page_location' : url,
+//        'page_title' : 'Contact'
+//    
+//    });
 }]);//Controller
